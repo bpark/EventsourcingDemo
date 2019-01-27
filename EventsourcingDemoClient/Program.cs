61a20a751015ca605f0d9e79c196fddd5035fd14
bpark <burt.parkers@gmail.com>
2019-01-27 20:27:46 +0100

    initial commit

---
+++ EventsourcingDemoClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using EventStore.ClientAPI;
+
+namespace EventsourcingDemoClient
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var connection =
+                EventStoreConnection.Create(new IPEndPoint(IPAddress.Parse("192.168.99.102"), 32001));
+
+            connection.ConnectAsync().Wait();
+
+            var myEvent = new EventData(Guid.NewGuid(), "testEvent", false,
+                Encoding.UTF8.GetBytes("some data"),
+                Encoding.UTF8.GetBytes("some metadata"));
+
+            connection.AppendToStreamAsync("test-stream",
+                ExpectedVersion.Any, myEvent).Wait();
+
+            var streamEvents =
+                connection.ReadStreamEventsForwardAsync("test-stream", 0, 1, false).Result;
+
+            var returnedEvent = streamEvents.Events[0].Event;
+
+            Console.WriteLine("Read event with data: {0}, metadata: {1}",
+                Encoding.UTF8.GetString(returnedEvent.Data),
+                Encoding.UTF8.GetString(returnedEvent.Metadata));
+        }
+    }
+}
